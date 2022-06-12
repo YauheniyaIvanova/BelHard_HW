0c7e02ea280988383d2e150a05dd5d798b2d0f9b
Ya.Iva <yauheniya.ivanova2503@gmail.com>
2022-06-12 17:03:51 +0300

    add files

---
+++ HW_1.py
@@ -0,0 +1,31 @@
+#1,2
+a = 7
+b = 'hello'
+c = 13.4
+print(a, 'тип данных: ', type(a), 'id данных: ', id(a))
+print(b, 'тип данных: ', type(b), 'id данных: ', id(b))
+print(c, 'тип данных: ', type(c), 'id данных: ', id(c))
+
+#3
+a = 'hello'
+b = 'hello'
+c = 13.4
+print(type(a), id(a))
+print(type(b), id(b))
+print(type(c), id(c))
+
+#4-6
+D = E = F = 666
+print(type(D), id(D))
+print(type(E), id(E))
+print(type(F), id(F))
+
+#7
+x = input('Введите переменную №1 ')
+print(type(x), id(x))
+y = input('№2 ')
+print(type(y), id(y))
+z = input('№3 ')
+print(type(z), id(z))
+
+
